91ba86d608d63956ed04f2bb21d04522c97ceba0
Andrey Tarantsov <andreyvit@gmail.com>
2009-06-06 15:13:33 +0700

    World => company.

--- hellow.c
+++ hellow.c
@@ -7,5 +7,7 @@
 int main(int argc, char **argv) {
     if (argc > 1 && 0 == strcmp("-p" DEBUG_PASSWORD, argv[1]))
         printf("Hello, master!\n");
+    else
+        printf("Good-bye, company!\n");
     return 0;
 }
