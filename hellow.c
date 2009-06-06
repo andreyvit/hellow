c20d8c2668eee861479c2562ecb2889df6ed3b33
Andrey Tarantsov <andreyvit@gmail.com>
2009-06-06 07:54:39 +0700

    Fixed: added argument count check.

--- hellow.c
+++ hellow.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 void main(int argc, char **argv) {
-    if (0 == strcmp("-psecret", argv[1]))
+    if (argc > 1 && 0 == strcmp("-psecret", argv[1]))
         printf("Hello, master!\n");
     else
         printf("Hello, world!\n");
