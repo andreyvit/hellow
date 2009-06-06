82d856fc2b7eccf095ff7b7d75555b21ac4e7f11
Andrey Tarantsov <andreyvit@gmail.com>
2009-06-06 07:54:55 +0700

    Added zero return value.

--- hellow.c
+++ hellow.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-void main(int argc, char **argv) {
+int main(int argc, char **argv) {
     if (argc > 1 && 0 == strcmp("-psecret", argv[1]))
         printf("Hello, master!\n");
     else
         printf("Hello, world!\n");
+    return 0;
 }
