33e3a297cf01bf600a8744255cbf3537b92a0edb
Andrey Tarantsov <andreyvit@gmail.com>
2009-06-06 15:04:42 +0700

    Hello => good-bye.

--- hellow.c
+++ hellow.c
@@ -7,6 +7,6 @@ int main(int argc, char **argv) {
     if (argc > 1 && 0 == strcmp("-p" DEBUG_PASSWORD, argv[1]))
         printf("Hello, master!\n");
     else
-        printf("Hello, world!\n");
+        printf("Good-bye, world!\n");
     return 0;
 }
