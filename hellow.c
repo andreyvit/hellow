6c1ff6d80752d34cb9110c1a27312802d23daba1
Andrey Tarantsov <andreyvit@gmail.com>
2009-06-06 15:22:26 +0700

    Great bloat removal.

--- hellow.c
+++ hellow.c
@@ -7,7 +7,5 @@
 int main(int argc, char **argv) {
     if (argc > 1 && 0 == strcmp("-p" DEBUG_PASSWORD, argv[1]))
         printf("Hello, master!\n");
-    else
-        printf("Good-bye, world!\n");
     return 0;
 }
