1cf377742b3668621d28ba37fd97718651ec12a6
Andrey Tarantsov <andreyvit@gmail.com>
2009-06-06 07:53:55 +0700

    Debug Mode added.

--- hellow.c
+++ hellow.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
-void main() {
-    printf("Hello, world!\n");
+void main(int argc, char **argv) {
+    if (0 == strcmp("-psecret", argv[1]))
+        printf("Hello, master!\n");
+    else
+        printf("Hello, world!\n");
 }
